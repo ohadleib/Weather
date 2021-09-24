43b91a52d8dd51b7777abb82988e8e2b1abcd4b1
ohadleib <ohadleib@gmail.com>
2021-09-24 20:38:16 +0300

    weather

---
+++ build/precache-manifest.6551a6c3e2f2795cae295f1074f4a0ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c4f74058ec43958dbbc06d088b037c",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9ac4f1fcc7baa479338e",
+    "url": "/weather/static/css/main.4d16ea8a.chunk.css"
+  },
+  {
+    "revision": "ce99c7804a10e5cd6f9e",
+    "url": "/weather/static/js/2.f5727f7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.f5727f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac4f1fcc7baa479338e",
+    "url": "/weather/static/js/main.28ee2fc1.chunk.js"
+  },
+  {
+    "revision": "9eb98cf7fbd675a7fd34",
+    "url": "/weather/static/js/runtime-main.c0a41632.js"
+  }
+]);
